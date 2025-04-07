2fa86fd33e796cc9109496733a86747e023a0df1
wallauerr <wallauer@protonmail.com>
2025-04-06 22:07:31 -0300

    fix: minimal adjusts

--- lib/player.ex
+++ lib/player.ex
@@ -1,14 +1,17 @@
 defmodule ExMon.Player do
-  @enforce_keys [:life, :name, :move_rnd, :move_avg, :move_heal]
-  defstruct [:life, :name, :move_rnd, :move_avg, :move_heal]
+  @require_keys [:life, :move_avg, :move_heal, :move_rnd, :name]
+  @max_life 100
+
+  @enforce_keys @require_keys
+  defstruct @require_keys
 
   def build(name, move_rnd, move_avg, move_heal) do
     %ExMon.Player{
-      name: name,
-      move_rnd: move_rnd,
+      life: @max_life,
       move_avg: move_avg,
       move_heal: move_heal,
-      life: 100
+      move_rnd: move_rnd,
+      name: name
     }
   end
 end
