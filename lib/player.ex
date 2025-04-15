0962a167ff73fad08f9c878dd8a8922ff3cbd5d9
wallauerr <wallauer@protonmail.com>
2025-04-15 20:24:04 -0300

    fix: adjust player moves to map with moves

--- lib/player.ex
+++ lib/player.ex
@@ -1,5 +1,5 @@
 defmodule ExMon.Player do
-  @require_keys [:life, :move_avg, :move_heal, :move_rnd, :name]
+  @require_keys [:life, :moves, :name]
   @max_life 100
 
   @enforce_keys @require_keys
@@ -8,9 +8,11 @@ defmodule ExMon.Player do
   def build(name, move_rnd, move_avg, move_heal) do
     %ExMon.Player{
       life: @max_life,
-      move_avg: move_avg,
-      move_heal: move_heal,
-      move_rnd: move_rnd,
+      moves: %{
+        rnd: move_rnd,
+        avg: move_avg,
+        heal: move_heal
+      },
       name: name
     }
   end
