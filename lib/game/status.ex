249e599ddd83c22e7c6477c1ab5b9bb94e83dc15
wallauerr <wallauer@protonmail.com>
2025-05-12 22:16:47 -0300

    feat: create message for heal skill

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -22,10 +22,14 @@ defmodule ExMon.Game.Status do
   end
 
   def print_move_message(:computer, :attack, damage) do
-    "\n-----The Player attacked the computer dealing #{damage} damage.-----\n"
+    IO.puts("\n-----The Player attacked the computer dealing #{damage} damage.-----\n")
   end
 
   def print_move_message(:player, :attack, damage) do
-    "\n-----The Computer attacked the player dealing #{damage} damage.-----\n"
+    IO.puts("\n-----The Computer attacked the player dealing #{damage} damage.-----\n")
+  end
+
+  def print_move_message(player, :heal, life) do
+    IO.puts("\n-----#{player} healed for #{life} life points.-----\n")
   end
 end
