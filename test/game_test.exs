933b4e2537b565e13b7db09d5ff00c05418366a2
wallauerr <wallauer@protonmail.com>
2025-05-18 23:05:45 -0300

    feat: create test to the game info func

--- test/game_test.exs
+++ test/game_test.exs
@@ -11,4 +11,30 @@ defmodule ExMon.GameTest do
       assert {:ok, _pid} = Game.start(computer, player)
     end
   end
+
+  describe "info/0" do
+    test "returns the current game state" do
+      player = Player.build("Wallauer", :chute, :soco, :cura)
+      computer = Player.build("Robot", :chute, :soco, :cura)
+
+      Game.start(computer, player)
+
+      expected_response = %{
+        status: :started,
+        player: %ExMon.Player{
+          name: "Wallauer",
+          life: 100,
+          moves: %{heal: :cura, avg: :soco, rnd: :chute}
+        },
+        computer: %ExMon.Player{
+          name: "Robot",
+          life: 100,
+          moves: %{heal: :cura, avg: :soco, rnd: :chute}
+        },
+        turn: :player
+      }
+
+      assert expected_response == Game.info()
+    end
+  end
 end
