7d1bd7fa57901e8a6556476c12e8f04c732367a1
wallauerr <wallauer@protonmail.com>
2025-05-19 21:26:37 -0300

    feat: create test to create player

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,7 +1,17 @@
 defmodule ExMonTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert true == true
+  alias ExMon.Player
+
+  describe "create_player/4" do
+    test "return a player" do
+      expected_response = %Player{
+        name: "Wallauer",
+        life: 100,
+        moves: %{heal: :cura, avg: :soco, rnd: :chute}
+      }
+
+      assert expected_response == ExMon.create_player("Wallauer", :chute, :soco, :cura)
+    end
   end
 end
