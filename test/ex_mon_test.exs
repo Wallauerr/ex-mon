4001dd4e3c6d11849713c92a65b07931b66d30e3
wallauerr <wallauer@protonmail.com>
2025-05-19 21:35:34 -0300

    feat: create test to the start game message

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,6 +1,8 @@
 defmodule ExMonTest do
   use ExUnit.Case
 
+  import ExUnit.CaptureIO
+
   alias ExMon.Player
 
   describe "create_player/4" do
@@ -12,6 +14,21 @@ defmodule ExMonTest do
       }
 
       assert expected_response == ExMon.create_player("Wallauer", :chute, :soco, :cura)
+    end
+  end
+
+  describe "start_game/1" do
+    test "when the game is started, returns a message" do
+      player = Player.build("Wallauer", :chute, :soco, :cura)
+
+      messages =
+        capture_io(fn ->
+          assert ExMon.start_game(player) == :ok
+        end)
+
+      assert messages =~ "The game is started!"
+      assert messages =~ "status: :started"
+      assert messages =~ "life: 100"
     end
   end
 end
